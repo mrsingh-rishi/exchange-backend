76dce0618ebbae5fd690b4e6aa25a67e0a6086a3
Rishi Singh <rs3949427@gmail.com>
2024-11-09 17:30:29 +0530

    complted: engine service

--- engine/src/index.ts
+++ engine/src/index.ts
@@ -1,4 +1,19 @@
+import { Engine } from "./trade/Engine";
+import { createClient } from "redis";
+
 async function main() {
-    
-    // const engine = 
+  const engine = new Engine();
+  const client = createClient();
+  await client.connect();
+
+  while (true) {
+    const response = await client.rPop("messagess" as string);
+
+    if (response) {
+      const message = JSON.parse(response);
+      engine.process(message);
+    }
+  }
 }
+
+main();
